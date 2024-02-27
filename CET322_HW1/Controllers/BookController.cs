6a9b8d7fa20ddbc806ca0a1dc6202fc8bd426e8a
Kağan ARTAN <kagan.artan@boun.edu.tr>
2024-02-28 02:39:01 +0300

    Add project files.

---
+++ CET322_HW1/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using CET322_HW1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+public class BookController : Controller
+{
+    public IActionResult Index()
+    {
+        // Örnek kitap kayıtları oluşturalım
+        var books = new List<CET322_HW1.Models.BookManager.Book>
+    {
+        new CET322_HW1.Models.BookManager.Book { BookName = "Denemeler", Author = "Montaigne", PageNumber = 336, DueDate = DateTime.Now },
+        new CET322_HW1.Models.BookManager.Book { BookName = "Suç ve Ceza", Author = "Dostoyevski", PageNumber = 551, DueDate = DateTime.Now.AddDays(-14) },
+        new CET322_HW1.Models.BookManager.Book { BookName = "1984", Author = "George Orwell", PageNumber = 328, DueDate = DateTime.Now.AddDays(21) }
+    };
+        return View(books);
+    }
+}
---
+++ CET322_HW1/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CET322_HW1.Models
+{
+    public class BookManager
+    {
+        public class Book
+        {
+            public string BookName { get; set; }
+            public string Author { get; set; }
+            public int PageNumber { get; set; }
+            public DateTime DueDate { get; set; }
+
+        }       
+    }  
+}
